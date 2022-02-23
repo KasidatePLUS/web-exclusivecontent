fdb41fb9e717c183e81affd9c2c02c76c68a69c4
kasidatep <kasidate.p@mail.kmutt.ac.th>
2022-02-23 18:21:04 +0700

    cheack word worng config kasp-block

--- assets/main/kasp-block.js
+++ assets/main/kasp-block.js
@@ -1,10 +1,10 @@
 window.addEventListener('contextmenu', function (e) {
     document.getElementById("rc-noti").innerHTML = UIkit.notification({
-        message:'<div class="uk-text-bold uk-text-small"><span uk-icon="warning"></span> KasidatePLUS NOTICFICATION</div>' +
+        message:'<div class="uk-text-bold uk-text-small"><span uk-icon="warning"></span> KasidatePLUS NOTIFICATION</div>' +
                 '<div class"uk-text-capitalize">Disable Right-Click</div>',
         status: 'warning',
         pos: 'bottom-right',
-        timeout: 10000
+        timeout: 1000
     });
     e.preventDefault();
 }, false);
