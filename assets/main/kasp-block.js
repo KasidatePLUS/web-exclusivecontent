049da4a8087ba00ed0403262b142c0d229adee95
kasidatep <kasidate.p@mail.kmutt.ac.th>
2022-02-23 13:17:25 +0700

    commit blog content to preview in spotlight

---
+++ assets/main/kasp-block.js
@@ -0,0 +1,23 @@
+window.addEventListener('contextmenu', function (e) {
+    document.getElementById("rc-noti").innerHTML = UIkit.notification({
+        message:'<div class="uk-text-bold uk-text-small"><span uk-icon="warning"></span> KasidatePLUS NOTICFICATION</div>' +
+                '<div class"uk-text-capitalize">Disable Right-Click</div>',
+        status: 'warning',
+        pos: 'bottom-right',
+        timeout: 10000
+    });
+    e.preventDefault();
+}, false);
+
+function killcopy(e) {
+    return false
+}
+function reEnable() {
+    return true
+}
+document.onselectstart = new function () {
+    if (window.sldebar) {
+        document.onmousedown = killcopy
+        document.onclick = reEnable
+    }
+}
