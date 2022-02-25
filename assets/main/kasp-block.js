416c8f61d5d82246546d66faddbb7ce199f5995f
kasidatep <kasidate.p@mail.kmutt.ac.th>
2022-02-25 18:57:51 +0700

    block copy ctrl c

--- assets/main/kasp-block.js
+++ assets/main/kasp-block.js
@@ -1,3 +1,4 @@
+
 window.addEventListener('contextmenu', function (e) {
     document.getElementById("kasp-noti").innerHTML = UIkit.notification({
         message:'<div class="uk-text-bold uk-text-small"><span uk-icon="warning"></span> KasidatePLUS NOTIFICATION</div>' +
@@ -9,3 +10,24 @@ window.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 }, false);
 
+window.addEventListener('copy', function (e) {
+    document.getElementById("kasp-noti").innerHTML = UIkit.notification({
+        message:'<div class="uk-text-bold uk-text-small"><span uk-icon="warning"></span> KasidatePLUS NOTIFICATION</div>' +
+                '<div class"uk-text-capitalize">Copying of text is not allowed. <code class="uk-label uk-label-warning">Ctrl</code> + <code class="uk-label uk-label-warning">C</code></div>',
+        status: 'warning',
+        pos: 'bottom-right',
+        timeout: 1000
+    });
+    e.preventDefault();
+}, false);
+
+window.addEventListener('online', function (e) {
+    document.getElementById("kasp-noti").innerHTML = UIkit.notification({
+        message:'<div class="uk-text-bold uk-text-small"><span uk-icon="warning"></span> KasidatePLUS NOTIFICATION</div>' +
+                '<div class"uk-text-capitalize">Out of focus <code class="uk-label uk-label-warning">Ctrl</code> + <code class="uk-label uk-label-danger">C</code></div>',
+        status: 'warning',
+        pos: 'bottom-right',
+        timeout: 100000
+    });
+    e.preventDefault();
+}, false);
