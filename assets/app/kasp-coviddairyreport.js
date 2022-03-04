5a1ffb6ed9f4ffab3c8aaf7b1f0480a1b4bf8272
kasidatep <kasidate.p@mail.kmutt.ac.th>
2022-03-04 15:36:14 +0700

    ads commit

--- assets/app/kasp-coviddairyreport.js
+++ assets/app/kasp-coviddairyreport.js
@@ -19,9 +19,12 @@ covidDairyReport.forEach(covidDairyReport => {
     let covidSegment = `
     <div class="cov">
     <h3 class="uk-card-title thai uk-text-bold">รายงานสถานการณ์โควิด-19 ประจำวันที่   <span class="cov-date uk-text-warning">  ${covidDairyReport.txn_date} </span></h3>
-      <div class="cov-newcase-title">  NEW CASE </div>
+    <div class="<div class="cov-newcase-title">  NEW CASE </div>
       <div class="cov-newcase">  ${covidDairyReport.new_case} Exclude abroad : ${covidDairyReport.new_case - covidDairyReport.new_case_excludeabroad}</div>
-      <div class="cov-recoveredcase">Recovered : + ${covidDairyReport.new_recovered} // total : ${covidDairyReport.total_recovered} // 1 jan 2022 = ${covidDairyReport.total_recovered - 2168494}</div>
+     "><div class="cov-newcase-title">  NEW CASE </div>
+    <div class="cov-newcase">  ${covidDairyReport.new_case} Exclude abroad : ${covidDairyReport.new_case - covidDairyReport.new_case_excludeabroad}</div>
+   </div>
+         <div class="cov-recoveredcase">Recovered : + ${covidDairyReport.new_recovered} // total : ${covidDairyReport.total_recovered} // 1 jan 2022 = ${covidDairyReport.total_recovered - 2168494}</div>
       <div class="cov-deathcase">Death : + ${covidDairyReport.new_death} // total : ${covidDairyReport.total_death} // 1 jan 2022 = ${covidDairyReport.total_death - 21698 }</div>
       <div class="cov-totalcase">Total Case : + ${covidDairyReport.total_case} // 1 jan 2022 = ${covidDairyReport.total_case - 2223435 }</div>
       <div class="cov-lastupdate"> Update : ${covidDairyReport.update_date}</div>
