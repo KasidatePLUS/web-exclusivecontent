99feef8e40559b8d09b8b3d3fec944427f3a1b51
kasidatep <kasidate.p@mail.kmutt.ac.th>
2022-03-05 14:14:25 +0700

    covid displau

--- assets/app/kasp-coviddairyreport.js
+++ assets/app/kasp-coviddairyreport.js
@@ -16,19 +16,50 @@ async function renderCovidData() {
 let covidDairyReport = await getCovidData();
 let covidsection = '';
 covidDairyReport.forEach(covidDairyReport => {
+   
+
     let covidSegment = `
-    <div class="cov">
-    <h3 class="uk-card-title thai uk-text-bold">รายงานสถานการณ์โควิด-19 ประจำวันที่   <span class="cov-date uk-text-warning">  ${covidDairyReport.txn_date} </span></h3>
-    <div class="<div class="cov-newcase-title">  NEW CASE </div>
-      <div class="cov-newcase">  ${covidDairyReport.new_case} Exclude abroad : ${covidDairyReport.new_case - covidDairyReport.new_case_excludeabroad}</div>
-     "><div class="cov-newcase-title">  NEW CASE </div>
-    <div class="cov-newcase">  ${covidDairyReport.new_case} Exclude abroad : ${covidDairyReport.new_case - covidDairyReport.new_case_excludeabroad}</div>
-   </div>
-         <div class="cov-recoveredcase">Recovered : + ${covidDairyReport.new_recovered} // total : ${covidDairyReport.total_recovered} // 1 jan 2022 = ${covidDairyReport.total_recovered - 2168494}</div>
-      <div class="cov-deathcase">Death : + ${covidDairyReport.new_death} // total : ${covidDairyReport.total_death} // 1 jan 2022 = ${covidDairyReport.total_death - 21698 }</div>
-      <div class="cov-totalcase">Total Case : + ${covidDairyReport.total_case} // 1 jan 2022 = ${covidDairyReport.total_case - 2223435 }</div>
+    <div class="cov uk-container uk-overlay uk-overlay-default" >
+    <div class="cov-title uk-panel uk-dark uk-margin-medium uk-text-center">
+      <h3 class="uk-card-title thai uk-text-bold ">รายงานสถานการณ์โควิด-19 ประจำวันที่ <span
+          class="cov-date uk-text-warning"> ${covidDairyReport.txn_date} </span></h3>
+    </div>
+    <div class="uk-grid-small uk-child-width-expand@m uk-text-center" uk-grid>
+      <div class="cov-section cov-newcase-section  uk-width-1-2 uk-card uk-card-default uk-card-body">
+        <div class="cov-newcase-title uk-text-bold thai"> ผู้ติดเชื้อรายใหม่ </div>
+        <div class="cov-newcase-data"> ${covidDairyReport.new_case} <span class="cov-rai-unit thai">ราย</span> </div>
+      </div>
+
+      <div class="cov-section cov-recoveredcase-section  uk-width-1-3 uk-card uk-card-default uk-card-body">
+        <div class="cov-recovered-title uk-text-bold thai"> หายป่วยวันนี้ </div>
+        <div class="cov-recovered-data"> ${covidDairyReport.new_recovered} <span class="cov-rai-unit thai">ราย</span></div>
+        <div class="cov-recovered-total thai"> หายป่วยสะสม :<span class="cov-recovered-total-data">
+            ${covidDairyReport.total_recovered - 2168494}</span> <span class="cov-rai-unit-second">ราย</span>
+             </div>
+      </div>
+
+      <div class="cov-section cov-deathcase-section uk-width-1-3 uk-card uk-card-default uk-card-body">
+        <div class="cov-death-title uk-text-bold thai"> เสียชีวิตวันนี้ </div>
+        <div class="cov-death-data"> ${covidDairyReport.new_death} <span class="cov-rai-unit thai">ราย</span></div>
+        <div class="cov-death-total thai"> เสียชีวิตสะสม :<span class="cov-death-total-data">
+            ${covidDairyReport.total_death - 21698 }</span> <span class="cov-rai-unit-second">ราย</span></div>
+      </div>
+      <div class="cov-section cov-totalcase-section uk-width-1-2 uk-card uk-card-default uk-card-body">
+        <div class="cov-total-title uk-text-bold thai"> ผู้ป่วยสะสม <span class="cov-totalcase-until"> ตั้งแต่ 1 มกราคม
+            2565</span> </div>
+        <div class="cov-total-data"> ${covidDairyReport.total_case - 2223435 } <span class="cov-rai-unit thai">ราย</span></div>
+        <div class="cov-all_total thai"> ป่วยสะสมนับตั้งแต่การระบาดครั้งแรก <span class="cov-all_total-data">
+            ${covidDairyReport.total_case}</span> <span class="cov-rai-unit-second">ราย</span></div>
+      </div>
+    </div>
+
+    <div class="cov-left">
+      <div class="cov-note thai"> หมายเหตุ : ข้อมูลตั้งแต่ 1 มกราคม 2565 </div>
+    </div>
+    <div class="cov-right">
       <div class="cov-lastupdate"> Update : ${covidDairyReport.update_date}</div>
     </div>
+  </div>
     `;
 
 covidsection += covidSegment;
@@ -39,3 +70,4 @@ covidcontainer.innerHTML = covidsection;
 }
 
 renderCovidData();
+
